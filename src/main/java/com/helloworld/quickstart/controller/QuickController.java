07566e58553fb875ee473064079e22d934396687
PARK-JONGSEOK <pjongsuk1@gmail.com>
2024-03-10 08:54:21 +0900

    GetMapping

--- src/main/java/com/helloworld/quickstart/controller/QuickController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -2,6 +2,8 @@ package com.helloworld.quickstart.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,22 @@ public class QuickController {
 		log.info("dummy");
 		return "{}";
 	}
+
 	@GetMapping("/dummy2")
 	public String dummy2() {
 		log.info("dummy2");
 		return "{}";
+	}
+
+	@GetMapping("/member")
+	public String getMember(@RequestParam("empNo") String empNo) {
+		log.info("empNo : {}", empNo);
+		return "ok";
+	}
+
+	@GetMapping("/company/{id}")
+	public String getCompany(@PathVariable("id") String id) {
+		log.info("empNo : {}", id);
+		return "ok";
 	}
 }
